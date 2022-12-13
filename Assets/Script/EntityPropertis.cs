0518725e1af0c80bf1b65c235b2b43ea81f6ea79
Marques Vivian <keiihostar@gmail.com>
2022-12-13 14:37:00 +0100

    BLABLA

---
+++ Assets/Script/EntityPropertis.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class EntityPropertis : MonoBehaviour
+{
+    private int i = 0;
+    public TextMeshProUGUI textYeux;
+    public TextMeshProUGUI textPeau;
+    public TextMeshProUGUI textCheveux;
+    public TextMeshProUGUI textSexe;
+    public TextMeshProUGUI textAge;
+    public string CouleurYeux;
+    public string CouleurPeau;
+    public string CouleurCheveux;
+    public int Sexe;
+    public int Age;
+    public string Nom;
+
+    public void TestViteFait(){
+        textYeux.text = CouleurYeux;
+        textPeau.text = CouleurPeau;
+        textCheveux.text = CouleurCheveux;
+        textSexe.text = ""+Sexe;
+        textAge.text = ""+Age;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class test : MonoBehaviour
+{
+           [SerializeField] 
+        public GameObject _title;
+
+        public void OnButtonClick()
+        {
+            //_title.text = "Your new text is here";
+        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
