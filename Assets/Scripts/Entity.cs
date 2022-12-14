32ff7e740e36e192e29f6e43b76c75016dc957a7
Sovahless <lucasmessio@protonmail.com>
2022-12-14 16:55:36 +0100

    ta mere

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public GameObject prefabEntity;
+    public Transform entitiesParent;
+
+    public TextMeshProUGUI nameProperty;
+    public TextMeshProUGUI hairProperty;
+    public TextMeshProUGUI sexProperty;
+    public TextMeshProUGUI eyesProperty;
+    public TextMeshProUGUI skinProperty;
+
+    private void Start()
+    {
+        
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(prefabEntity, entitiesParent);
+        }
+        nameProperty.text = "NAME";
+        hairProperty.text = "HAIR";
+        sexProperty.text = "SEX";
+        eyesProperty.text = "EYES";
+        skinProperty.text = "SKIN";
+    }
+    
+    public void AttributeValues(string name, string hair, string sex, string eyes, string skin)
+    {
+        nameProperty.text = name;
+        hairProperty.text = hair;
+        sexProperty.text = sex;
+        eyesProperty.text = eyes;
+        skinProperty.text = skin;
+    }
+}
