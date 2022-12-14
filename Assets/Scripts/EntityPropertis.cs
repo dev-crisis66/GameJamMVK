f5270e95389a327da55eaca73b7f7203d9965c1f
dev-crisis66 <kevin.bardou@lidem.education>
2022-12-14 15:16:15 +0100

    add entity prefab + properties

--- Assets/Scripts/EntityPropertis.cs
+++ Assets/Scripts/EntityPropertis.cs
@@ -1,15 +1,17 @@
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 
 public class EntityPropertis : MonoBehaviour
 {
     
-    public TextMeshProUGUI textYeux;
-    public TextMeshProUGUI textPeau;
-    public TextMeshProUGUI textCheveux;
-    public TextMeshProUGUI textSexe;
-    public TextMeshProUGUI textAge;
-    public TextMeshProUGUI textId;
+    // public TextMeshProUGUI textYeux;
+    // public TextMeshProUGUI textPeau;
+    // public TextMeshProUGUI textCheveux;
+    // public TextMeshProUGUI textSexe;
+    // public TextMeshProUGUI textAge;
+    // public TextMeshProUGUI textId;
+    private GameObject ValueProperties;
     public string CouleurYeux;
     public string CouleurPeau;
     public string CouleurCheveux;
@@ -20,17 +22,18 @@ public class EntityPropertis : MonoBehaviour
 
     public void ViewStats(){
         
-        textYeux.text = CouleurYeux;
-        textPeau.text = CouleurPeau;
-        textCheveux.text = CouleurCheveux;
-        textSexe.text = ""+Sexe;
-        textAge.text = ""+Age;
-        textId.text=""+idEntity;
+        // Debug.Log(ValueProperties);
+        // textYeux.text = CouleurYeux;
+        // textPeau.text = CouleurPeau;
+        // textCheveux.text = CouleurCheveux;
+        // textSexe.text = ""+Sexe;
+        // textAge.text = ""+Age;
+        // textId.text=""+idEntity;
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        ValueProperties = GameObject.FindWithTag("ValueProperties");
         
     }
 
